a3e5aba3fecb6ad1b2f7b2715a561a0df6d991f9
Dmitry Makhnev <dmitry.makhnev@jetbrains.com>
2021-04-28 21:42:15 +0300

    Fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
@@ -11,5 +12,8 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', { ignoreComments: true }],
   },
+  plugins: ['jest'],
 };
