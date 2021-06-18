47cbfc2ebc49c2a500ddc547af4c544ac83c05f5
Vasiliy Vanchuk <v.vanchuk@tut.by>
2021-06-18 20:36:40 +0300

    Setup typescript for eslint

--- src/sum.ts
+++ src/sum.ts
@@ -1,9 +1,9 @@
-export const sum = (a: number|string, b: number|string): number  => {
+export const sum = (a: number | string, b: number | string): number => {
   if (typeof a === "number" && typeof b === "number") {
     return a + b;
   }
-  let numberA: number = typeof a !== "number" ? Number(a) : a;
-  let numberB: number = typeof b !== "number" ? Number(b) : b;
+  const numberA: number = typeof a !== "number" ? Number(a) : a;
+  const numberB: number = typeof b !== "number" ? Number(b) : b;
 
   return numberA + numberB;
 };
